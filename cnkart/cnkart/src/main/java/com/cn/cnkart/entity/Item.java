dea4c991c0dc55c8608fdb4d42f4d93debbfcdd9
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2025-01-01 12:24:38 +0530

    Basic Hibernate Connection

---
+++ cnkart/cnkart/src/main/java/com/cn/cnkart/entity/Item.java
@@ -0,0 +1,39 @@
+package com.cn.cnkart.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
